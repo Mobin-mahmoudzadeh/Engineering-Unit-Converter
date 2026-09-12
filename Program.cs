d85d7773f502dbb7f6b9949735031716511eb0a0
Mobin-mahmoudzadeh <mobin.mahoudzadehhh@gmail.com>
2026-09-12 12:32:07 +0330

    Add basic Celsius to Fahrenheit converter

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace program
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter temperature in Calsius: ");
+      string input = Console.ReadLine();
+      double celsius = Convert.ToDouble(input);
+      
+      double fahrenheit = (celsius * 9 / 5) + 32;
+      
+      Console.WriteLine("Temperature in Fahrenheit is:" + fahrenheit);
+    }
+  }
+};
+
