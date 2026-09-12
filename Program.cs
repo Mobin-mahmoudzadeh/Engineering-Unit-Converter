fe6d51500869bc2b3b23d2323147cbd65327a70e
Mobin-mahmoudzadeh <mobin.mahoudzadehhh@gmail.com>
2026-09-12 13:24:53 +0330

    feat: add two-way temperature conversion menu with input validation

--- Program.cs
+++ Program.cs
@@ -6,13 +6,51 @@ namespace program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Enter temperature in Calsius: ");
+      Console.WriteLine("1. Converting Celsius to Fahrenheit");
+      Console.WriteLine("2. Converting Fahrenheit to Celsius");
+      Console.Write("Please select your option: ");
       string input = Console.ReadLine();
-      double celsius = Convert.ToDouble(input);
-      
-      double fahrenheit = (celsius * 9 / 5) + 32;
-      
-      Console.WriteLine("Temperature in Fahrenheit is:" + fahrenheit);
+
+      switch (input)
+      {
+        case "1":
+        {
+          Console.Write("Enter temperature in Celsius: ");
+          string tempInput = Console.ReadLine();
+          
+          if (double.TryParse(tempInput, out double celsius))
+          {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine("Temperature in Fahrenheit is: " + fahrenheit);
+          }
+          else
+          {
+            Console.WriteLine("Invalid temperature value!");
+          }
+          break;
+        }
+        case "2":
+        {
+          Console.Write("Enter temperature in Fahrenheit: ");
+          string tempInput = Console.ReadLine();
+          
+          if (double.TryParse(tempInput, out double fahrenheit))
+          {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Temperature in Celsius is: " + celsius);
+          }
+          else
+          {
+            Console.WriteLine("Invalid temperature value!");
+          }
+          break;
+        }
+        default:
+        {
+          Console.WriteLine("Invalid option! Please run the program again.");
+          break;
+        }
+      }
     }
   }
 };
