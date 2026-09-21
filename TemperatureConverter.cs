3d7982163b8acffa314148f86f56f9ae36158700
Mobin-mahmoudzadeh <mobin.mahoudzadehhh@gmail.com>
2026-09-21 13:40:39 +0330

    feat: add ConvertCelsiusToFahrenheit with absolute zero guard clause

---
+++ TemperatureConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace program;
+
+public class TemperatureConverter
+{
+    public double ConvertCelsiusToFahrenheit(double temperature)
+    {
+        if (temperature < -273.15)
+        {
+            throw new ArgumentOutOfRangeException(nameof(temperature), "Temperature cannot be below absolute zero (-273.15°C).");
+        }
+
+        return (temperature * 9 / 5) + 32;
+    }
+}
